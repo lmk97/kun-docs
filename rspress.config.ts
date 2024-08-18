ada8edb03aa87191d6068958802e4417ababb7c3
lmk97 <kun.js@outlook.com>
2024-08-18 12:06:10 +0800

    fix: config

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: 'docs',
-  outDir: 'pages',
+  outDir: '../kun-pages',
   base: 'kun-docs',
   title: 'Kun',
   description: 'An incredibly fast JavaScript runtime.',
